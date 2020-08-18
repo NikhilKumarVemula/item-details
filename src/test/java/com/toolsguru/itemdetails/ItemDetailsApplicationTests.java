4ce2cf8164c0d04cc8a4af40330b27ae65357d23
ashish-vemula <vemulaashishkumar@gmail.com>
2020-08-17 22:24:12 -0400

    Intial H2 DB setup for item-details project

---
+++ src/test/java/com/toolsguru/itemdetails/ItemDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsguru.itemdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
