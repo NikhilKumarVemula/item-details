0ed8c6ae291c54f2e15b16482251df84dfc3006f
ashish-vemula <vemulaashishkumar@gmail.com>
2020-08-19 00:10:15 -0400

    Added Crocs anotation

--- src/main/java/com/toolsguru/itemdetails/controller/ItemDetailsController.java
+++ src/main/java/com/toolsguru/itemdetails/controller/ItemDetailsController.java
@@ -3,11 +3,13 @@ package com.toolsguru.itemdetails.controller;
 import com.toolsguru.itemdetails.model.Category;
 import com.toolsguru.itemdetails.service.ItemDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class ItemDetailsController {
 
